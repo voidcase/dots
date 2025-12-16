e399906164fb2e6d37cea012ae34157e2316e261
Isak Lindhé <isak.lindhe@maxiv.lu.se>
2025-12-16 10:30:27 +0100

    lsp completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,26 +3,99 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.termguicolors = true
-
+vim.o.autoindent = true
+vim.o.cursorline = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
+vim.lsp.config["pylsp"] = {
+    cmd = { 'pylsp' },
+    filetypes = { 'python' },
+    root_markers = {
+        'pyproject.toml', 'setup.py', 'setup.cfg',
+        'requirements.txt', 'Pipfile', '.git',
+    },
+    settings = {}, -- see https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+}
+vim.lsp.enable("pylsp")
 
+-- from https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
+vim.lsp.config["lua_ls"] = {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  root_markers = {
+    '.emmyrc.json', '.luarc.json', '.luarc.jsonc',
+    '.luacheckrc', '.stylua.toml', 'stylua.toml',
+    'selene.toml', 'selene.yml', '.git',
+  },
+  settings = {
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true, semicolon = 'Disable' },
+    },
+  },
+}
+vim.lsp.enable("lua_ls")
+
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+-- vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+-- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+-- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+vim.keymap.set('n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>')
+-- vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+require("bootstrap_lazy")
 require("lazy").setup({
     "tpope/vim-surround",
+    "tpope/vim-fugitive",
+    "numToStr/Comment.nvim",
     "folke/which-key.nvim",
-    "airblade/vim-gitgutter",
+    "ellisonleao/gruvbox.nvim",
+    "RRethy/vim-illuminate",
     {
-        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+        branch = 'master',
+        lazy = false,
+        build = ":TSUpdate",
         config = function()
-            vim.lsp.enable("pylsp")
+            require 'nvim-treesitter.configs'.setup { ensure_installed = {"python"} }
         end
-    }
+
+    },
+    {
+        "airblade/vim-gitgutter",
+        config = function()
+            vim.api.nvim_create_autocmd({"BufWritePost"}, {command="GitGutter"})
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+            "hrsh7th/cmp-buffer",   -- Buffer source
+            "hrsh7th/cmp-path",     -- Path source
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+
+            })
+        end,
+    },
 })
+
+vim.cmd([[colorscheme gruvbox]])
